1bef1596241f8787bbd233a3d9c00a9e6dd7d753
Sthefany <159933039+sthefanycv@users.noreply.github.com>
2024-04-16 10:11:23 -0400

    pt-1

---
+++ instalacion/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    // Aqui podemos definir nuestras clases personalizadas
+    extend: {
+      // clases personalizadas
+      fontFamily:{
+        'arvo': ['moderna', 'sans-serif'],
+        'ttl': ['Titillium Web', 'sans-serif'],
+      },
+      width:{
+        '100pct': '100%',
+      },
+      height:{
+        '100alto': '100vh',
+        'altoSection': 'calc(100vh - 235px)',
+      },
+    },
+  },
+  plugins: [],
+}
